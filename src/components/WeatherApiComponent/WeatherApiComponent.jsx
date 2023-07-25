1ec79001b6f58080b049cacdbfa99bc47de46b4a
nagaraj6618 <nagaraj516700@gmail.com>
2023-07-25 22:44:37 +0530

    CSS ADDED

--- src/components/WeatherApiComponent/WeatherApiComponent.jsx
+++ src/components/WeatherApiComponent/WeatherApiComponent.jsx
@@ -15,7 +15,18 @@ function WeatherApiComponent() {
         
         <button type='submit' className='btn'>Search</button>
       </div>
-      
+      <div className='container'>
+        <div class="shape1"></div>
+        <div class="shape2"></div>
+        <div className='container-card'>
+          <h2 className='head-text'>Chennai</h2>
+          <div className='wrapper'>
+            <p>Temperature : 20 c</p>
+            <p>Humidity : 33.5</p>
+            <p>Wind Speed : 12 km/hr</p>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }

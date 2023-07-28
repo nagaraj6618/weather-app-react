ae4c62fd109623af00dc057c1a999159c8e8a43c
nagaraj6618 <nagaraj516700@gmail.com>
2023-07-28 22:38:09 +0530

    Added css for search suggestion

--- src/components/WeatherApiComponent/WeatherApiComponent.jsx
+++ src/components/WeatherApiComponent/WeatherApiComponent.jsx
@@ -1,23 +1,31 @@
 import React, { useState } from 'react'
 import Axios from 'axios'
-import "./WeatherApiComponent.css"
+
+import "./WeatherComponent.css"
 import CITIES from '../../data/cities-name-list'
 
 function WeatherApiComponent() {
+  const [data,setData] = useState([])
+  const [dataCheck,setDataCheck] = useState(null)
   const [searchQuery,setSearchQuery] = useState("")
 
-  const [data,setData] = useState([null])
+  
   const [main,setMain] = useState([])
   const [wind,setWind] = useState([])
+  
 
-
-
+  const inputHandler = (event) => {
+    setSearchQuery(event.target.value);
+   
+    
+  }
   const searchHandler = async() => {
     try{
       const response = await Axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${searchQuery}&APPID=3cba5e74adf3dff51d692eac46d71e49`)
       setData(response.data);
       setMain(response.data.main);
-      setWind(response.data.wind)
+      setWind(response.data.wind);
+      setDataCheck(true)
       
     }
     catch(err){
@@ -25,20 +33,20 @@ function WeatherApiComponent() {
     }
   }
   return (
+    <div>
     <div className='header'>
+      
       <div className='search-bar'>
         <input
         className='search' 
         type='text'
         placeholder='Search'
         value={searchQuery}
-        onChange={(e)=>setSearchQuery(e.target.value)}
+        onChange={inputHandler }
         />
         
         <button type='submit' className='btn' onClick={searchHandler}>Search</button>
-      </div>
-      
-      <div className='search-container'>
+        <div className='search-container'>
        
         {CITIES.filter(item => {
           const searchItem = searchQuery.toLowerCase()
@@ -52,7 +60,11 @@ function WeatherApiComponent() {
         ))}
         
       </div>
-      {data && (
+      </div>
+      </div>
+      
+  
+      {dataCheck && (
       
         <div className='container'>
           <div className="shape1"></div>

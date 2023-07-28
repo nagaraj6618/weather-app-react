661068b4ded7c5f24263f6ce7b6fd97f4e6d77b9
nagaraj6618 <nagaraj516700@gmail.com>
2023-07-28 12:18:04 +0530

    Added css for search suggestion

--- src/components/WeatherApiComponent/WeatherApiComponent.jsx
+++ src/components/WeatherApiComponent/WeatherApiComponent.jsx
@@ -1,18 +1,16 @@
 import React, { useState } from 'react'
 import Axios from 'axios'
 import "./WeatherApiComponent.css"
-// import CountryCode from '../../data/countryCode'
+import CITIES from '../../data/cities-name-list'
+
 function WeatherApiComponent() {
   const [searchQuery,setSearchQuery] = useState("")
-  // const [option,setOption] = useState("")
+
   const [data,setData] = useState([null])
   const [main,setMain] = useState([])
   const [wind,setWind] = useState([])
 
-  // const optionHandler = (event) =>{
-  //   setOption(event.target.value)
-  //   console.log(option);
-  // }
+
 
   const searchHandler = async() => {
     try{
@@ -36,33 +34,23 @@ function WeatherApiComponent() {
         value={searchQuery}
         onChange={(e)=>setSearchQuery(e.target.value)}
         />
-        {/* <select onChange={optionHandler}
-        className='Select'>
-
-          {CountryCode.map((option) => 
-            (<option key={option.code}>{option.name}</option>))
-          }
-          
-        </select> */}
+        
         <button type='submit' className='btn' onClick={searchHandler}>Search</button>
       </div>
+      
       <div className='search-container'>
-        <div>hey</div>
-        <div>good morning</div>
-        <div>hloooo</div>
-        <div>welcome</div>
-        <div>hey</div>
-        <div>good morning</div>
-        <div>hloooo</div>
-        <div>welcome</div>
-        <div>hey</div>
-        <div>good morning</div>
-        <div>hloooo</div>
-        <div>welcome</div>
-        <div>hey</div>
-        <div>good morning</div>
-        <div>hloooo</div>
-        <div>welcome</div>
+       
+        {CITIES.filter(item => {
+          const searchItem = searchQuery.toLowerCase()
+          const fullName = item.toLowerCase()
+          return searchItem && fullName.startsWith(searchItem) && fullName !== searchItem
+        })
+        
+        .map((data) => (
+          <div onClick={()=>setSearchQuery(data)} >{data}</div>
+         
+        ))}
+        
       </div>
       {data && (
       

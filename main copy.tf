4d3b9a1c6cb765a046de3c7fca7c5b2e065cb043
NAGARAJ S <nagaraj516700@gmail.com>
2024-08-08 22:53:42 +0530

    Terraform checking -9

--- main copy.tf
+++ main copy.tf
@@ -4,17 +4,22 @@ resource "local_file" "npm_install_script" {
   content = <<-EOT
     @echo off
 
+    REM Remove the build files 
+    rmdir /s /q D:\Devops\weather-app\weather-app-react\build
+
+    REM Remove the node_modules folder
+    rmdir /s /q D:\Devops\weather-app\weather-app-react\node_modules
+
     REM Navigate to the React project directory
     cd D:\Devops\weather-app\weather-app-react
 
     REM Install npm dependencies and build the project
-    npm run build
+    npm install && npm run build
 
     REM Copy build to deploy directory
     xcopy "D:\Devops\weather-app\weather-app-react\build" "D:\Devops\weather-app\weather-app-react\deploy" /E /I /Y
-    REM Copy
-    cd .
   EOT
   filename = "${path.module}/install_and_build.bat"
   file_permission = "0755"
 }
+
--- terraform/install_and_build.bat
+++ terraform/install_and_build.bat
@@ -1,13 +1,12 @@
 @echo off
 
-REM Remove the build files 
-rmdir /s /q D:\Devops\weather-app\weather-app-react\build
-
-REM remove the build files 
-rmdir /s /q D:\Devops\weather-app\weather-app-react\node_modules
-
 REM Navigate to the React project directory
 cd D:\Devops\weather-app\weather-app-react
 
-REM Install npm dependencies
-npm install && npm install -g serve && npm run build 
+REM Install npm dependencies and build the project
+npm run build
+
+REM Copy build to deploy directory
+xcopy "D:\Devops\weather-app\weather-app-react\build" "D:\Devops\weather-app\weather-app-react\deploy" /E /I /Y
+REM Copy
+cd .

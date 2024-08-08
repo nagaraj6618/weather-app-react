ae3cedfd34c11fb6ccedb3afb0832f1fec66f012
NAGARAJ S <nagaraj516700@gmail.com>
2024-08-08 17:05:53 +0530

    Terraform checking - 7

--- terraform/main.tf
+++ terraform/main.tf
@@ -3,14 +3,18 @@ provider "local" {}
 resource "local_file" "npm_install_script" {
   content = <<-EOT
     @echo off
+
+    REM Remove the build files 
+    rmdir /s /q D:\Devops\weather-app\weather-app-react\build
+
+    REM remove the build files 
+    rmdir /s /q D:\Devops\weather-app\weather-app-react\node_modules
+
     REM Navigate to the React project directory
     cd D:\Devops\weather-app\weather-app-react
 
     REM Install npm dependencies
-    npm install && npm run build
-
-    REM Build the React project
-    npm run build
+    npm install && npm run build && serve -s build
   EOT
   filename = "${path.module}/install_and_build.bat"
   file_permission = "0755"
